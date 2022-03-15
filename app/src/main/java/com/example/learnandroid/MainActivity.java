489e27a4d30bbf330f691a0c90f142479ec41e7e
m6ydz642 <m6ydz642@gmail.com>
2022-03-15 23:58:48 +0900

    서브 엑티비티 이동

--- app/src/main/java/com/example/learnandroid/MainActivity.java
+++ app/src/main/java/com/example/learnandroid/MainActivity.java
@@ -16,45 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button webbutton = (Button)findViewById(R.id.webbutton);
-        Button dialbutton = (Button)findViewById(R.id.dialbutton);
-        Button gallerybutton = (Button)findViewById(R.id.gallerybutton);
-        Button exitbutton = (Button)findViewById(R.id.exitbutton);
 
-
-        webbutton.setBackgroundColor(Color.GRAY);
-        webbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mintent = new Intent(Intent.ACTION_VIEW, Uri.parse(("http://www.naver.com")));
-                startActivity(mintent);
-            }
-        });
-
-        dialbutton.setBackgroundColor(Color.GREEN);
-        dialbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mintent = new Intent(Intent.ACTION_VIEW, Uri.parse(("tel:/911")));
-                startActivity((mintent));
-            }
-        });
-
-        gallerybutton.setBackgroundColor(Color.RED);
-        gallerybutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mintent = new Intent(Intent.ACTION_VIEW, Uri.parse(("content://media/internal/images/media")));
-                startActivity(mintent);
-            }
-        });
-        exitbutton.setBackgroundColor(Color.YELLOW);
-        exitbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
 
     }
 }
