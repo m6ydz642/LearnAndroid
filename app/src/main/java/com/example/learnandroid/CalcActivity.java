895ef338d61886597360d01fbdb13970a3228605
m6ydz642 <m6ydz642@gmail.com>
2022-03-23 23:07:53 +0900

    계산 결과 추가

--- app/src/main/java/com/example/learnandroid/CalcActivity.java
+++ app/src/main/java/com/example/learnandroid/CalcActivity.java
@@ -1,14 +1,32 @@
 package com.example.learnandroid;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CalcActivity extends AppCompatActivity {
+
+    EditText edit1, edit2;
+    Button btnAdd, btnSub, btnMul, btnDiv;
+    TextView textResult;
+    String num1, num2;
+    Integer result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
+
+        setTitle("초간단 계산기");
+
+        edit1 = (EditText) findViewById(R.id.Edit1);
+        edit2 = (EditText) findViewById(R.id.Edit2);
+        btnAdd = (Button) findViewById(R.id.BtnAdd);
+        textResult = (TextView) findViewById(R.id.TextResult);
+
     }
 
 }
