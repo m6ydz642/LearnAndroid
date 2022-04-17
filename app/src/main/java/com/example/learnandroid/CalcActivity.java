6abb9d765c269d2ee4656d62245523836e34d4ed
m6ydz642 <m6ydz642@gmail.com>
2022-04-17 22:40:32 +0900

    입력 값 없을 때 경고 추가 (나머지 버튼은 안함)

--- app/src/main/java/com/example/learnandroid/CalcActivity.java
+++ app/src/main/java/com/example/learnandroid/CalcActivity.java
@@ -1,11 +1,13 @@
 package com.example.learnandroid;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -31,7 +33,48 @@ public class CalcActivity extends AppCompatActivity {
         btnMul = (Button) findViewById(R.id.BtnMinus);
         btnDiv = (Button) findViewById(R.id.BtnSplit);
         textResult = (TextView) findViewById(R.id.TextResult);
+        String msg = "값을 입력해주세요";
+        // OnClick이벤트로 변경
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
 
+
+                if (!TextUtils.isEmpty(edit1.getText().toString()) && !TextUtils.isEmpty(edit2.getText().toString())  ){
+                    // 나머지 조건은 그냥 안함
+                    num1 = edit1.getText().toString();
+                    num2 = edit2.getText().toString();
+                    result = Integer.parseInt(num1) + Integer.parseInt(num2);
+                    textResult.setText("계산결과 : " + result.toString());
+                }else Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0){
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1) - Integer.parseInt(num2);
+                textResult.setText("계산결과 : " + result.toString());
+            }
+        });
+        btnMul.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0){
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1) * Integer.parseInt(num2);
+                textResult.setText("계산결과 : " + result.toString());
+            }
+        });
+
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0){
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1) / Integer.parseInt(num2);
+                textResult.setText("계산결과 : " + result.toString());
+            }
+        });
 
 /*        btnAdd.setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(View arg0, MotionEvent arg1){
@@ -72,41 +115,7 @@ public class CalcActivity extends AppCompatActivity {
             }
         });*/
 
-        // OnClick이벤트로 변경
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view){
-                num1 = edit1.getText().toString();
-                num2 = edit2.getText().toString();
-                result = Integer.parseInt(num1) + Integer.parseInt(num2);
-                textResult.setText("계산결과 : " + result.toString());
-            }
-        });
 
-        btnSub.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0){
-                num1 = edit1.getText().toString();
-                num2 = edit2.getText().toString();
-                result = Integer.parseInt(num1) - Integer.parseInt(num2);
-                textResult.setText("계산결과 : " + result.toString());
-            }
-        });
-        btnMul.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0){
-                num1 = edit1.getText().toString();
-                num2 = edit2.getText().toString();
-                result = Integer.parseInt(num1) * Integer.parseInt(num2);
-                textResult.setText("계산결과 : " + result.toString());
-            }
-        });
-
-        btnDiv.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0){
-                num1 = edit1.getText().toString();
-                num2 = edit2.getText().toString();
-                result = Integer.parseInt(num1) / Integer.parseInt(num2);
-                textResult.setText("계산결과 : " + result.toString());
-            }
-        });
 
 
     }
