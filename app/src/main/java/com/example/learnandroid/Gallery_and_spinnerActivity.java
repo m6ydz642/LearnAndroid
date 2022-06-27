28233c564a654cdfabbc0d93880337d4de0f4ba0
m6ydz642 <m6ydz642@gmail.com>
2022-06-27 21:18:46 +0900

    갤러리 완료

--- app/src/main/java/com/example/learnandroid/Gallery_and_spinnerActivity.java
+++ app/src/main/java/com/example/learnandroid/Gallery_and_spinnerActivity.java
@@ -1,7 +1,13 @@
 package com.example.learnandroid;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Gallery;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +20,56 @@ public class Gallery_and_spinnerActivity extends AppCompatActivity {
         setTitle("갤러리 영화 포스터");
         Gallery gallery = (Gallery)findViewById(R.id.gallery1);
         MyGalleryAdapater gallyadapter = new MyGalleryAdapater(this);
-        // gallyadapter.setAdapter
+        gallery.setAdapter(gallyadapter);
+    }
+
+
+    // 중첩클래스
+    public class MyGalleryAdapater extends BaseAdapter {
+
+        Context context;
+        Integer[] posterId = {R.drawable.movie1,R.drawable.movie2,R.drawable.movie3,R.drawable.movie4 };
+        public MyGalleryAdapater(Context context){
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return posterId.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+            ImageView imageview = new ImageView(context);
+            imageview.setLayoutParams(new Gallery.LayoutParams(200, 300));
+            imageview.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageview.setPadding(5,5,5,5);
+            imageview.setImageResource(posterId[i]);
+
+            // final int pos = i;
+
+            imageview.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+
+                    ImageView ivPoster = (ImageView)findViewById(R.id.ivPoster2);
+                    ivPoster.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                    ivPoster.setImageResource(posterId[i]);
+                    return false;
+                }
+            });
+            return imageview;
+        }
     }
 }
--- app/src/main/java/com/example/learnandroid/MyGalleryAdapater.java
+++ app/src/main/java/com/example/learnandroid/MyGalleryAdapater.java
@@ -1,19 +1,26 @@
+/*
+Gallery_and_spnnerActivity 에서 중첩클래스로 넣음
 package com.example.learnandroid;
 
 import android.content.Context;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
 
 public class MyGalleryAdapater extends BaseAdapter {
 
+    Context context;
+    Integer[] posterId = {R.drawable.movie1,R.drawable.movie2,R.drawable.movie3,R.drawable.movie4 };
     public MyGalleryAdapater(Context context){
-
+        this.context = context;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return posterId.length;
     }
 
     @Override
@@ -28,6 +35,27 @@ public class MyGalleryAdapater extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+
+        ImageView imageview = new ImageView(context);
+        imageview.setLayoutParams(new Gallery.LayoutParams(200, 300));
+        imageview.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        imageview.setPadding(5,5,5,5);
+        imageview.setImageResource(posterId[i]);
+
+       // final int pos = i;
+
+        imageview.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Gallery_and_spinnerActivity gallery_and_spinnerActivity = (Gallery_and_spinnerActivity)context;
+                ImageView ivPoster = (ImageView)gallery_and_spinnerActivity.findViewById(R.id.ivPoster2);
+              //  ImageView ivPoster = (ImageView)findViewById(R.id.ivPoster2); // 이방식으로 쓰고싶으면 중첩클래스로 Gallery_and_spnnerActivity에 넣으면 됨
+                ivPoster.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                ivPoster.setImageResource(posterId[i]);
+                return false;
+            }
+        });
+        return imageview;
     }
 }
+*/
