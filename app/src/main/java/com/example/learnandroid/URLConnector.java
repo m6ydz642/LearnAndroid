1a4da44ca62191fb859cef8ef98be0f360018cb4
m6ydz642 <m6ydz642@gmail.com>
2022-08-02 12:47:02 +0900

    import 추가

--- app/src/main/java/com/example/learnandroid/URLConnector.java
+++ app/src/main/java/com/example/learnandroid/URLConnector.java
@@ -3,6 +3,7 @@ package com.example.learnandroid;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
